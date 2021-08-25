2b52823f7042d1180892e566854129b2cd31029d
Dylan Winger <20423@my4county.net>
2021-08-25 09:53:18 -0400

    Finished product

---
+++ src/asciiart/AsciiArt.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asciiart;
+
+/**
+ *
+ * @author dwinger
+ */
+public class AsciiArt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    System.out.println("     ,      ,    ");
+    System.out.println("    (\\_____/)    ");
+    System.out.println("      (_00_)   ");
+    System.out.println("        (0)      ");
+    System.out.println("      __| |__   \\)");
+    System.out.println("   []/_______\\[]/ ");
+    System.out.println("   / \\______/ \\/");
+    System.out.println("  /    /__\\   ");
+    System.out.println(" (\\   /____\\  ");
+   
+    
+    }
+    
+}
